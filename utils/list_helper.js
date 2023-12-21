a14c1e4eb850fd834a8e13df84c7c567fd1100c4
Muhammad Usama <usamajhamat111@gmail.com>
2023-12-21 14:44:44 +0500

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+
+  const maxLikesBlog = blogs.reduce((maxLikes, blog) =>
+    blog.likes > maxLikes.likes ? blog : maxLikes
+  );
+  return {
+    title: maxLikesBlog.title,
+    author: maxLikesBlog.author,
+    likes: maxLikesBlog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
