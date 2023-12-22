fcb0eafa8de946e4232370da3f3b10ff1c4a963b
Muhammad Usama <usamajhamat111@gmail.com>
2023-12-22 11:20:14 +0500

    4.6-4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -21,8 +21,30 @@ const favoriteBlog = (blogs) => {
   };
 };
 
+const mostBlogs = (blogs) => {
+  if(blogs.length === 0){
+    return { author: undefined, blogs: undefined };
+  }
+  const blogsCountMap = blogs.reduce((countMap, blog) => {
+    countMap[blog.author] = (countMap[blog.author] || 0) + 1;
+    return countMap; 
+  }, {});
+
+  const topAuthor = Object.keys(blogsCountMap).reduce(
+    (maxAuthor, author)=>
+      blogsCountMap[author] > blogsCountMap[maxAuthor] ? author : maxAuthor ,
+      Object.keys(blogsCountMap)[0]
+    
+  )
+  return {
+    author : topAuthor,
+    blogs: blogsCountMap[topAuthor]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs
 };
